63e88a82fe6b9cf212a1883314a645e3d308c7f6
amp <am.perescu@gmail.com>
2021-05-13 16:38:36 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <app-header/>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AppHeader
+  }
+}
+</script>
+
+<style lang="scss">
+@import "./assets/scss/style.scss";
+</style>
