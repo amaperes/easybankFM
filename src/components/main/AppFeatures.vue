87987ec4a5689dd9da58a20b27f5b4ecc1453ea4
amp <am.perescu@gmail.com>
2021-05-25 19:31:15 +0300

    design features section

---
+++ src/components/main/AppFeatures.vue
@@ -0,0 +1,64 @@
+<template>
+  <section class="feature">
+    <div class="container container--p-all">
+      <div class="feature__intro">
+        <h2>
+        Why choose Easybank?
+        </h2>
+        <p>
+          We leverage Open Banking to turn your bank account into your financial hub. Control
+          your finances like never before.
+        </p>
+      </div>
+      <div class="feature__grid">
+        <div class="feature__item">
+          <div class="feature__icon"><img src="../../assets/images/icon-online.svg"/></div>
+          <div class="feature__title">
+            <h3> Online Banking </h3>
+          </div>
+          <div class="feature__description">
+            Our modern web and mobile applications allow you to keep track of your finances
+            wherever you are in the world.
+          </div>
+        </div>
+
+        <div class="feature__item">
+          <div class="feature__icon"><img src="../../assets/images/icon-budgeting.svg"/></div>
+          <div class="feature__title">
+            <h3> Simple Budgeting </h3>
+          </div>
+          <div class="feature__description">
+            See exactly where your money goes each month. Receive notifications when you’re
+            close to hitting your limits.
+          </div>
+        </div>
+
+        <div class="feature__item">
+          <div class="feature__icon"><img src="../../assets/images/icon-onboarding.svg"/></div>
+          <div class="feature__title">
+            <h3> Fast Onboarding </h3>
+          </div>
+          <div class="feature__description">
+            We don’t do branches. Open your account in minutes online and start taking control
+            of your finances right away.
+          </div>
+        </div>
+
+        <div class="feature__item">
+          <div class="feature__icon"><img src="../../assets/images/icon-api.svg"/></div>
+          <div class="feature__title">
+            <h3> Open API </h3>
+          </div>
+          <div class="feature__description">
+            Manage your savings, investments, pension, and much more from one account. Tracking
+            your money has never been easier.
+          </div>
+        </div>
+      </div>
+  </div>
+</section>
+</template>
+
+<style lang="scss" scoped>
+@import '../../assets/scss/pages/features.scss'
+</style>
