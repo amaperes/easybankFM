829dc00cfb00292bf489eac216ea00acbac41152
amp <am.perescu@gmail.com>
2021-05-26 14:46:14 +0300

    design articles section

--- src/App.vue
+++ src/App.vue
@@ -2,25 +2,7 @@
   <app-header/>
   <app-hero />
   <app-features />
-  By Claire Robinson
-  Receive money in any currency with no fees
-  The world is getting smaller and we’re becoming more mobile. So why should you be
-  forced to only receive money in a single …
-
-  By Wilson Hutton
-  Treat yourself without worrying about money
-  Our simple budgeting feature allows you to separate out your spending and set
-  realistic limits each month. That means you …
-
-  By Wilson Hutton
-  Take your Easybank card wherever you go
-  We want you to enjoy your travels. This is why we don’t charge any fees on purchases
-  while you’re abroad. We’ll even show you …
-
-  By Claire Robinson
-  Our invite-only Beta accounts are now live!
-  After a lot of hard work by the whole team, we’re excited to launch our closed beta.
-  It’s easy to request an invite through the site ...
+  <app-articles />
 
   About Us
   Contact
@@ -43,13 +25,15 @@
 import AppHeader from './components/AppHeader.vue'
 import AppHero from './components/main/AppHero.vue'
 import AppFeatures from './components/main/AppFeatures.vue'
+import AppArticles from './components/main/AppArticles.vue'
 
 export default {
   name: 'App',
   components: {
     AppHeader,
     AppHero,
-    AppFeatures
+    AppFeatures,
+    AppArticles
   }
 }
 </script>
